937fe82eb40e5543b7a07b88f29091c2773b3b3d
Lukas Krickl <lukas@krickl.dev>
2023-08-16 05:26:44 +0200

    WIP: generic vec and mime type handling

--- include/libqc/mime.h
+++ include/libqc/mime.h
@@ -15,6 +15,8 @@ enum qc_mime_conds {
   QC_MIME_COND_BIN_CONTAINS,
   QC_MIME_COND_STARTS_WITH_TEXT,
   QC_MIME_COND_CONTAINS_TEXT,
+  // in case there are mime types that require more complex
+  // logic it can be done using a custom callback
   QC_MIME_COND_CUSTOM,
 };
 
@@ -22,7 +24,8 @@ enum qc_mime_conds {
 // a struct is *either* of a certain type or it is not
 // There is no complex logic in determining a type otherwise
 struct qc_mime_cond {
-  int prio; // lower == higher priority
+  // lower == higher priority
+  int prio;
   enum qc_mime_conds type;
   union {
     const char *ext;
@@ -31,16 +34,17 @@ struct qc_mime_cond {
 };
 
 struct qc_mime {
-  int prio;
   const char *type;
   qc_mime_print print;
+  // lower == higher priority
+  // if determine finds a better mime type
+  // with higher priority it will
+  // consequently replace the current mime type
+  int prio;
 };
 
 struct qc_mime qc_mime_determine(struct qc_handle *handle, const char *path,
-                                 const char *content);
-
-// TODO: how do we register mime types?
-void qc_mime_register(struct qc_handle *handle);
+                                 const char *content, int prev_prio);
 
 void qc_mime_free(const struct qc_mime *self);
 
---
+++ include/libqc/vec.h
@@ -0,0 +1,6 @@
+#ifndef VEC_H_
+#define VEC_H_
+
+#include <stddef.h>
+
+#endif
